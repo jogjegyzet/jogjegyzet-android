a27b3edd6334dd35a9a46d4f64580665dfe479e1
Dániel Gergely <gergelydaniel@gmail.com>
2019-01-09 04:58:26 +0100

    PDF Reader styling

--- app/src/main/java/com/gergelydaniel/jogjegyzet/ui/reader/ReaderController.kt
+++ app/src/main/java/com/gergelydaniel/jogjegyzet/ui/reader/ReaderController.kt
@@ -29,6 +29,7 @@ class ReaderController(private val url: String, private val docTitle: String) : 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         pdfView = PDFView(inflater.context, null)
         pdfView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        pdfView.setBackgroundColor(0xFF666666.toInt())
         return pdfView
     }
 
@@ -36,14 +37,13 @@ class ReaderController(private val url: String, private val docTitle: String) : 
     override fun onAttach(view: View) {
         super.onAttach(view)
 
-        //val temp = url.replace("https://api.jogjegyzet.hu/", "http://192.168.0.23:8080/")
-
         Observable.fromCallable { URL(url).openStream() }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle())
                 .subscribe {
                     pdfView.fromStream(it)
+                            .spacing(8)
                             .onPageError { i, t ->
                                 t.printStackTrace()
                             }

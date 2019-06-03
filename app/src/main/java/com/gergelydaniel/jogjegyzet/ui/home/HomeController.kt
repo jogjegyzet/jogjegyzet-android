4b2aeacad7879545537a4a12b13f2f1612f76f35
Dániel Gergely <gergelydaniel@gmail.com>
2019-06-03 20:09:33 +0200

    Cards refactor

--- app/src/main/java/com/gergelydaniel/jogjegyzet/ui/home/HomeController.kt
+++ app/src/main/java/com/gergelydaniel/jogjegyzet/ui/home/HomeController.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.christianbahl.conductor.ConductorInjection
 import com.gergelydaniel.jogjegyzet.R
-import com.gergelydaniel.jogjegyzet.domain.Category
-import com.gergelydaniel.jogjegyzet.domain.Document
 import com.gergelydaniel.jogjegyzet.domain.NoInternetException
 import com.gergelydaniel.jogjegyzet.service.DocumentData
 import com.gergelydaniel.jogjegyzet.ui.AdapterClickListener
@@ -15,7 +13,6 @@ import com.gergelydaniel.jogjegyzet.ui.BaseController
 import com.gergelydaniel.jogjegyzet.ui.adapter.BrowserAdapter
 import com.gergelydaniel.jogjegyzet.ui.adapter.ViewHolder
 import com.gergelydaniel.jogjegyzet.util.Either
-import com.gergelydaniel.jogjegyzet.util.dp
 import com.gergelydaniel.jogjegyzet.util.vis
 import kotlinx.android.synthetic.main.controller_home.view.*
 import javax.inject.Inject
@@ -24,8 +21,8 @@ class HomeController : BaseController() {
     @Inject
     lateinit var presenter: HomePresenter
 
-    private lateinit var categoriesAdapter: BrowserAdapter
-    private lateinit var favoritesAdapter: FavoritesAdapter
+    private lateinit var categoriesAdapter: CardBrowserAdapter
+    private lateinit var favoritesAdapter: CardBrowserAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         return inflater.inflate(R.layout.controller_home, container, false)
@@ -39,13 +36,13 @@ class HomeController : BaseController() {
     override fun onAttach(view: View) {
         super.onAttach(view)
 
-        categoriesAdapter = BrowserAdapter()
+        categoriesAdapter = CardBrowserAdapter()
         view.list_categories.adapter = categoriesAdapter
 
         val adapterClickListener = AdapterClickListener(router)::onAdapterClick
         categoriesAdapter.onClickListener = adapterClickListener
 
-        favoritesAdapter = FavoritesAdapter()
+        favoritesAdapter = CardBrowserAdapter()
         view.list_favorites.adapter = favoritesAdapter
 
         favoritesAdapter.onClickListener = adapterClickListener
@@ -64,6 +61,8 @@ class HomeController : BaseController() {
 
         view.category_progress.vis = vm.categories is CategoriesViewModel.Loading
         view.card_favorites.vis = vm.categories is CategoriesViewModel.Data && vm.favorites.isNotEmpty()
+
+        view.card_categories.vis = vm.categories is CategoriesViewModel.Data
 
         view.error.vis = vm.categories is CategoriesViewModel.Error
         view.empty.vis = vm.categories is CategoriesViewModel.Data && vm.categories.categories.isEmpty()
@@ -84,10 +83,6 @@ class HomeController : BaseController() {
             }
         }
 
-        //var favs = vm.favorites.joinToString("\n") { it.name }
-        //if (favs.isEmpty()) favs = "Nincs kedvenc"
-        //view.text_favorites.text = favs
-
         favoritesAdapter.data = vm.favorites.map { Either.Right(DocumentData(it, true)) }
     }
 
@@ -101,7 +96,7 @@ class HomeController : BaseController() {
 
 }
 
-private class FavoritesAdapter: BrowserAdapter() {
+private class CardBrowserAdapter: BrowserAdapter() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         holder.itemView.setBackgroundColor(0xFFFFFFFF.toInt())
